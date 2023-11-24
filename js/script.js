0262cb78e41fb482c5cd509dad213053b30c1f2c
matheusdcunha <matheus.cunha.fonseca50@gmail.com>
2023-11-24 17:09:38 -0300

    Custom Controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-import { SlideNav } from "./slide.js";
+import SlideNav from "./slide.js";
 
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
 slide.changeSlide(0);
-slide.addControl();
+slide.addControl(".custom-controls");
